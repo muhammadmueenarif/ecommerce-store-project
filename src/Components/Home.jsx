2090187ac9ab86fc68d0ab60567f8e2d68a64bfe
Mueen Arif <muhammadmueenarif786@gmail.com>
2025-01-11 23:28:39 +0500

    styled home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Home.css'
+import imgheader from "./images/01.webp"
 
 const Home = () => {
   return (
     <div>
-      This is home page
+      <div className='home_header'>
+        <div className='data'>
+        <div className="header_container">
+          <h1>Hair</h1>
+          <p>Starting from top, we covered range of formulas, to keep your hair 
+            in tip-top shape and style in mint condition</p>
+            <button className="Shampoobtn">Shop Shampoo</button>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
