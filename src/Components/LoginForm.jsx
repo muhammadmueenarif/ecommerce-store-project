fa29e7244a60b5ac50213b1fe0c0c8733037d7a4
Mueen Arif <muhammadmueenarif786@gmail.com>
2025-01-12 00:33:47 +0500

    login page design

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
+import './Loginup.css'
+import { Password } from '@mui/icons-material'
 
+const account1={
+  username: "john",
+  pin: "1111",
+}
+const account2={
+  username: "david",
+  pin: "2222",
+}
+const account3={
+  username: "carlos",
+  pin: "3333",
+}
+const account4={
+  username: "mathew",
+  pin: "4444",
+} 
+
+const accounts = [account1, account2, account3, account4]
 const LoginForm = () => {
+  
+  const [username, setusername] = useState("")
+  const [pin, setpin] = useState("")
+  const [error, seterror] = useState("")
+
   return (
     <div>
-      This is login form
+      <form className='sign-in-form' action=''>
+
+        <label>Username
+        <input type="text" value={username} />
+        </label>
+
+        <label>Password
+        <input type="text" value={pin} />
+        </label>
+        <br />
+        <button className='loginbtn' type='submit'>Login</button>
+      <div className="error">{error && <div>{error}</div>}</div>
+      </form>
     </div>
   )
 }
